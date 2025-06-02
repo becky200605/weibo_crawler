766261e92a066c23acca6a6f4b400a00751f2611
becky200605 <lidehua0821@gmail.com>
2025-06-02 22:06:51 +0800

    😊 搭建基本项目框架

---
+++ etc/settings.py
@@ -0,0 +1,21 @@
+DB_CONFIG={
+    "host":"localhost",
+    "port":3306,
+    "user":"your_user",
+    "password":"your_password",
+    "database":"weibo_analysis"
+}
+PROXY = {
+    "server": "http://your_proxy_ip:port",
+    "username": "user",
+    "password": "pass"
+}
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 Chrome/114.0.0.0"
+}
+
+CRAWL_OPTIONS = {
+    "pages_per_topic": 2,
+    "delay": 2
+}
